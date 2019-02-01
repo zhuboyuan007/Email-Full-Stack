3da4346d5d5252928a7734a33123ec581c365175
boyuan zhu <uwzhuboyuan@gmail.com>
2019-01-31 23:13:06 -0500

    sync

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "bye" });
-});
+passport.use(
+    new googleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken,refreshToken, profile, done) => {
+        console.log('accessToken: ', accessToken);
+        console.log('refreshToekn', refreshToken);
+        console.log(profile);
+    })  
+);
 
-app.get("/en", (req, res) => {
-  console.log("m here");
-  res.send({ hi: "en" });
-});
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
