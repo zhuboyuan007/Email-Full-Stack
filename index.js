31ca4be15dae79f3d80421bb6f270f31c987b77e
boyuan zhu <uwzhuboyuan@gmail.com>
2019-02-16 21:44:13 -0500

    refact

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const express = require("express");
-const passport = require("passport");
-const googleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
 
-passport.use(
-    new googleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    }, (accessToken,refreshToken, profile, done) => {
-        console.log('accessToken: ', accessToken);
-        console.log('refreshToekn', refreshToken);
-        console.log(profile);
-    })  
-);
-
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
+
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken: ", accessToken);
+      console.log("refreshToken: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
